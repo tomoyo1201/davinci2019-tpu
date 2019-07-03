148b03a292d3adaead1db69345845e479ff10440
TomoyoHirai <tomoyotomo1201@gmail.com>
2019-07-03 14:10:34 +0900

    kiojk

--- public/js_client/index.js
+++ public/js_client/index.js
@@ -138,7 +138,10 @@ window.onload = () => {
 
 document.onkeydown = function (evt){
     // 既存の処理中略
+    var keyCode = event.keyCode;
+     if (keyCode == 13) {
     document.getElementById("subsend").classList.add("pressing");
+  }
 }
 document.onkeyup = function (evt){
     // 既存の処理中略
